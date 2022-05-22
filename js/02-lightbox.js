c934cb2150bb0a3b14ddd7821b73e7dceb6fc478
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-05-22 08:33:18 +0200

    test

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,10 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.body.querySelector(".gallery");
+const markup = galleryItems.map((image) => `<a class="gallery__item" href="${image.original}">
+                                            <img class="gallery__image" src=${image.preview} alt=${image.description}
+                                            </a>`).join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData:"alt", captionDelay:250 });
